daf6e2139082efa29ddcf90763f780f117ebb2b7
Ronery Filho <roneryrffilho@gmail.com>
2023-06-03 20:05:58 -0300

    adicionado styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { SafeAreaView, Text } from 'react-native';
-('');
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 25px;
+  color: cyan;
+`;
+
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Testando</Text>
+      <Text style={styles.container}>Testando123</Text>
+      <TextNew>NOVO TESTE</TextNew>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    color: 'green',
+  }
+})
 
 export default App;
