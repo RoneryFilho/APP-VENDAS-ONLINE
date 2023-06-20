8116f3c15ef276f01435a84c8333a0eb2abfb054
Ronery Filho <roneryrffilho@gmail.com>
2023-06-20 12:57:05 -0300

    Aprendendo FlatList

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,81 @@
-import { NativeSyntheticEvent, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TextInputChangeEventData } from 'react-native';
+import { FlatList, NativeSyntheticEvent, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TextInputChangeEventData } from 'react-native';
 import styled from 'styled-components/native';
 import Example from './Example';
 import { useState } from 'react';
 
 
 const App = () => {
-const [valor, setValor] = useState<string>('');
 
-const handleOnChangeInput = (valor: NativeSyntheticEvent<TextInputChangeEventData>) => {
-  setValor(valor.nativeEvent.text);
-}
+const list: string [] = [
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+  'teste',
+]
 
   return (
     <SafeAreaView>
-      <ScrollView style={styles.scroll}>
-        <Text style={styles.container}>Testando</Text>
-        <Text style={styles.container}>Testando</Text>
-        <Text style={styles.container}>Testando</Text>
-        <Text style={styles.container}>Testando</Text>
-        <Text style={styles.container}>Testando</Text>
-        <Text style={styles.container}>Testando</Text>
-        <Text style={styles.container}>Testando</Text>
-        <Text style={styles.container}>Testando</Text>
-      </ScrollView>
-      <Example text='Texto Original'>Exemplo Children</Example>
-      <Example text='Texto Original 2'>Exemplo Children 2</Example>
-      <TextInput onChange={handleOnChangeInput} value={valor} style={styles.input}></TextInput>
-      <Text style={styles.container}>Valor input: {valor}</Text>
+      <FlatList style={styles.flat} data={list} renderItem={({item}) => <Text style={styles.container}>{item}</Text>}/>
     </SafeAreaView>
   );
 };
@@ -35,16 +84,10 @@ const styles = StyleSheet.create({
   container: {
     color: 'black',
     fontSize: 30,
-    backgroundColor: 'aqua'
+    backgroundColor: '#ddd'
   },
-  input: {
-    backgroundColor:'#ddd',
-    margin: 16,
-    fontSize: 20
-  },
-  scroll: {
-    backgroundColor: 'aqua',
-    height: 150,
+  flat: {
+    backgroundColor: 'blue',
   },
 })
 
